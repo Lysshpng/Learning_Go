04da44aa5acef54f9bc98e4e95eab7f9e0c8566c
lysshpng <997189603@qq.com>
2025-06-27 15:47:30 +0800

    learning 0627

--- README.md
+++ README.md
@@ -9,9 +9,11 @@
 - [x] Go环境
     > 1. 安装Go;
     > 2. 安装插件网络问题解决: 
-    >   >   go env -w GO111MODULE=on <br/>
+    >   >   go env -w GO111MODULE=on (但是要off掉后才能git) <br/>
     >   >   go env -w GOPROXY=https://proxy.golang.com.cn,direct
 
 - [ ] 学习Golang基础
-    > go mod init Learning_Go 初始化Go项目
-    > [first Go](src/hello.go)
+    > go mod init Learning_Go 初始化Go项目 <br/>
+    > [first Go](src/hello.go) <br/>
+    > 基础语法与Python相近 <br/>
+    > 
--- src/hello.go
+++ src/hello.go
@@ -7,9 +7,52 @@ import "fmt"
 // main函数是每一个可执行程序所必须包含的
 // 在main之前可能还有一个init
 func main() {
+	// { 不能单独放在一行, 在运行时会产生错误
 	fmt.Println("Hello, World!")
 	// Println 带回车
 	// Print 不带回车
+
+	// 格式化字符串
+	// %d 表示整型数字，%s 表示字符串
+	var stockcode = 123
+	var enddate = "2020-12-31"
+	var url = "Code=%d&endDate=%s"
+	// 使用Sprintf
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
+	fmt.Println(target_url)
+	// 使用Printf
+	fmt.Printf(url, stockcode, enddate)
+	fmt.Println()
+
+	// 变量
+	// var name typer
+	var intA int
+	intA = 1
+	fmt.Println(intA)
+	// var name typer = value
+	var strB string = "test"
+	fmt.Println(strB)
+	// var name = value 根据值自行判定变量类型
+	var boolC = true
+	fmt.Println(boolC)
+	// name := value
+	floatC := 1.234
+	fmt.Println(floatC)
+
+	// 多变量声明
+	// var name1, name2, name3 typer
+	var name1, name2, name3 bool
+	name1, name3 = true, false
+	name4, name5 := true, false
+	fmt.Println(name1, name2, name3, name4, name5)
+
+	// 变量类型默认值
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
 }
 
 /**
